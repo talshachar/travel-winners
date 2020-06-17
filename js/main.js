9b5d40a87c5c77879f007a0688019df7363f1453
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-17 16:21:40 +0300

    added weather

--- js/main.js
+++ js/main.js
@@ -30,10 +30,11 @@ window.onload = () => {
         .catch(err => {
             console.log('err!!!', err);
         })
-    weatherService.getWeather(gCurrLoc.lat,gCurrLoc.lng)
-    .then(weather=>{
-        console.log(weather)
-    })
+    weatherService.getWeather(gCurrLoc.lat, gCurrLoc.lng)
+        .then(weather => {
+            console.log(weather)
+            renderWeather(weather)
+        })
 
 }
 
@@ -41,3 +42,10 @@ document.querySelector('.btn').addEventListener('click', (ev) => {
     console.log('Aha!', ev.target);
     mapService.panTo(35.6895, 139.6917);
 })
+
+
+function renderWeather(weather) {
+    document.querySelector('.temp').innerHTML = 'Temp: '+weather.main.temp+'&#8451;';
+    document.querySelector('.wind-speed').innerText = 'Wind: '+ weather.wind.speed;
+    document.querySelector('.general-weather-desc').innerText = weather.weather[0].description ;
+}

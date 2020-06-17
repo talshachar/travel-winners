8b2f96a9e2fb4ef4d68401fdd7be6bda68d0bf40
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-17 16:59:11 +0300

    refactor weather

--- js/main.js
+++ js/main.js
@@ -30,11 +30,8 @@ window.onload = () => {
         .catch(err => {
             console.log('err!!!', err);
         })
-    weatherService.getWeather(gCurrLoc.lat, gCurrLoc.lng)
-        .then(weather => {
-            console.log(weather)
-            renderWeather(weather)
-        })
+        renderWeather()
+
 
 }
 
@@ -44,9 +41,13 @@ document.querySelector('.btn').addEventListener('click', (ev) => {
 })
 
 
-function renderWeather(weather) {
-    document.querySelector('.weather-location').innerText='in ' +weather.name;
-    document.querySelector('.temp').innerHTML = 'Temp: '+weather.main.temp+'&#8451;';
-    document.querySelector('.wind-speed').innerText = 'Wind: '+ weather.wind.speed;
-    document.querySelector('.general-weather-desc').innerText = weather.weather[0].description ;
+function renderWeather() {
+    weatherService.getWeather(gCurrLoc.lat, gCurrLoc.lng)
+        .then(weather => {
+            document.querySelector('.weather-location').innerText = 'in ' + weather.name;
+            document.querySelector('.temp').innerHTML = 'Temp: ' + weather.main.temp + '&#8451;';
+            document.querySelector('.wind-speed').innerText = 'Wind: ' + weather.wind.speed + ' m/s';
+            document.querySelector('.general-weather-desc').innerText = weather.weather[0].description;
+        })
+
 }

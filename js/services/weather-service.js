ac7845fe61120584f1e262fb4271b7d598de803e
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-18 08:17:05 +0300

    forced https on weather

--- js/services/weather-service.js
+++ js/services/weather-service.js
@@ -8,7 +8,7 @@ var W_KEY = '54b1e010ed0f97537666a296504f7ba4';
 
 
 function getWeather(lat, lng) {
-    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${W_KEY}`)
         .then(res => res.data)
 }
 
